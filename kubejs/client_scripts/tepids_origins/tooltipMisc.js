71f7437069a5ab6e8cd302f593edcdaf313c3bed
tepid <tepschmep@gmail.com>
2024-04-27 18:16:58 -0300

    `[/]` Modified tooltip structure

--- kubejs/client_scripts/tepids_origins/tooltipMisc.js
+++ kubejs/client_scripts/tepids_origins/tooltipMisc.js
@@ -1,21 +1,23 @@
 ItemEvents.tooltip(e => {
 
     e.addAdvanced(["tepids_origins:origin_essence"], (item, advanced, text) => {
-        text.add(1, [Text.of("Used to upgrade ").gray(), Text.of("Origin Items").gold(), Text.of(" and craft ").gray(), Text.of("Orbs of Origin").gold(), Text.of(".").gray()])
+        text.add(1, [Text.of("Used to upgrade ").gray(), Text.of("Origin Items").white(), Text.of(" and craft ").gray(), Text.of("Orbs of Origin").white(), Text.of(".").gray()])
     })
 
     e.addAdvanced(["tepids_origins:evolution_encyclopedia"], (item, advanced, text) => {
-        text.add(1, [Text.of("This item functions like an ").gray(), Text.of("Orb of Origin").gold(), Text.of(".").gray()])
-        text.add(2, Text.gray(" "))
-        text.add(3, [Text.of("Additionally resets all ").gray(), Text.of("Skill Trees").gold(), Text.of(" for all Origins.").gray()])
+        text.add(1, [Text.of("Functions like an ").gray(), Text.of("Orb of Origin").white(), Text.of(".").gray()])
+        text.add(2, [Text.of("Additionally resets all ").gray(), Text.of("Skill Trees").white(), Text.of(" for all Origins.").gray()])
     })
 
     e.addAdvanced(["#tepids_origins:golden_crests"], (item, advanced, text) => {
-        text.add(1, [Text.of("Used to craft ").gray(), Text.of("Origin Items").gold(), Text.of(" and ").gray(), Text.of("Skill Books").gold(), Text.of(".").gray()])
+
+        text.add(1, [Text.of("Used to craft ").gray(), Text.of("Origin Items").white(), Text.of(" and ").gray(), Text.of("Skill Books").white(), Text.of(".").gray()])
         text.add(2, Text.gray(" "))
+
         if (!e.shift) {
-            text.add(3, [Text.of("Hold ").gray(), Text.of("Shift").gold(), Text.of(" for more information.").gray()])
+            text.add(3, [Text.gray("  ▲ Hold ["), Text.white("Shift"), Text.gray("]")])
         } else {
+            text.add(3, [Text.gray("  ▼ Hold ["), Text.white("Shift"), Text.gray("]")])
             switch (item.id) {
                 case "tepids_origins:golden_low_crest": { labelText = "Low Impact"; break; }
                 case "tepids_origins:golden_medium_crest": { labelText = "Medium Impact"; break; }
@@ -27,17 +29,20 @@ ItemEvents.tooltip(e => {
                 case "tepids_origins:golden_utility_crest": { labelText = "Utility"; break; }
                 case "tepids_origins:golden_exploration_crest": { labelText = "Exploration"; break; }
             }
-            text.add(3, [Text.of("This item is related to ").darkPurple(), Text.of(labelText).lightPurple(), Text.of(" origins.").darkPurple()])
-            text.add(4, [Text.of("Texture by ").gray(), Text.of("Fancybread").gold(), Text.of(".").gray()])
+            text.add(4, [Text.of("    This item is related to ").gray(), Text.of(labelText).white(), Text.of(" origins.").gray()])
         }
+
     })
 
     e.addAdvanced(["#tepids_origins:netherite_crests"], (item, advanced, text) => {
-        text.add(1, [Text.of("Used to upgrade ").gray(), Text.of("Origin Items").gold(), Text.of(" and craft ").gray(), Text.of("Origin Essence").gold(), Text.of(".").gray()])
+
+        text.add(1, [Text.of("Used to upgrade ").gray(), Text.of("Origin Items").white(), Text.of(" and craft ").gray(), Text.of("Origin Essence").white(), Text.of(".").gray()])
         text.add(2, Text.gray(" "))
+
         if (!e.shift) {
-            text.add(3, [Text.of("Hold ").gray(), Text.of("Shift").gold(), Text.of(" for more information.").gray()])
+            text.add(3, [Text.gray("  ▲ Hold ["), Text.white("Shift"), Text.gray("]")])
         } else {
+            text.add(3, [Text.gray("  ▼ Hold ["), Text.white("Shift"), Text.gray("]")])
             switch (item.id) {
                 case "tepids_origins:netherite_low_crest": { labelText = "Low Impact"; break; }
                 case "tepids_origins:netherite_medium_crest": { labelText = "Medium Impact"; break; }
@@ -49,17 +54,20 @@ ItemEvents.tooltip(e => {
                 case "tepids_origins:netherite_utility_crest": { labelText = "Utility"; break; }
                 case "tepids_origins:netherite_exploration_crest": { labelText = "Exploration"; break; }
             }
-            text.add(3, [Text.of("This item is related to ").darkPurple(), Text.of(labelText).lightPurple(), Text.of(" origins.").darkPurple()])
-            text.add(4, [Text.of("Texture by ").gray(), Text.of("Fancybread").gold(), Text.of(".").gray()])
+            text.add(4, [Text.of("    This item is related to ").gray(), Text.of(labelText).white(), Text.of(" origins.").gray()])
         }
+
     })
 
     e.addAdvanced(["#tepids_origins:skill_books"], (item, advanced, text) => {
-        text.add(1, [Text.of("Grants ").gray(), Text.of("Skill Points").gold(), Text.of(" to certain origins at the cost of ").gray(), Text.of("XP").gold(), Text.of(".").gray()])
+
+        text.add(1, [Text.of("Grants ").gray(), Text.of("Skill Points").white(), Text.of(" to certain origins at the cost of ").gray(), Text.of("XP").white(), Text.of(".").gray()])
         text.add(2, Text.gray(" "))
+
         if (!e.shift) {
-            text.add(3, [Text.of("Hold ").gray(), Text.of("Shift").gold(), Text.of(" for more information.").gray()])
+            text.add(3, [Text.gray("  ▲ Hold ["), Text.white("Shift"), Text.gray("]")])
         } else {
+            text.add(3, [Text.gray("  ▼ Hold ["), Text.white("Shift"), Text.gray("]")])
             switch (item.id) {
                 case "tepids_origins:low_skill_book": { labelText = "Low Impact"; break; }
                 case "tepids_origins:medium_skill_book": { labelText = "Medium Impact"; break; }
@@ -71,9 +79,9 @@ ItemEvents.tooltip(e => {
                 case "tepids_origins:utility_skill_book": { labelText = "Utility"; break; }
                 case "tepids_origins:exploration_skill_book": { labelText = "Exploration"; break; }
             }
-            text.add(3, [Text.of("This item grants skill points to ").darkPurple(), Text.of(labelText).lightPurple(), Text.of(" origins.").darkPurple()])
-            text.add(4, [Text.of("Texture by ").gray(), Text.of("Fancybread").gold(), Text.of(".").gray()])
+            text.add(4, [Text.of("    This item grants skill points to ").gray(), Text.of(labelText).white(), Text.of(" origins.").gray()])
         }
+
     })
 
 })
